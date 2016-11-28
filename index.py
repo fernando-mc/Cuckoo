e21ab9ff2690c1260a6f4cde27e7b300302264da
Fernando Medina Corey <fernando@curalate.com>
2016-11-28 01:01:05 -0500

    Intermediary commit - needs changes

---
+++ index.py
@@ -0,0 +1,133 @@
+import json
+import logging
+import recipients
+import datetime
+import boto3 # Make sure you have installed locally
+from jinja2 import Template # Install from requirements.txt
+
+logging.basicConfig()
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+FROM_ADDRESS='staff@thewoofgarden.com'
+REPLY_TO_ADDRESS='staff@thewoofgarden.com'
+
+TODAY = DAYS[datetime.date.today().weekday()]
+DAYS = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
+BASE_TASKS = ('1. Feed the dogs \n2. Feed the rabbits \n'
+             + '3. Feed the cats \n4. Feed the turtles \n' 
+             + '5. Walk the dogs \n6. Empty cat litterboxes\n')
+DAILY_TASKS = {
+    'Monday': BASE_TASKS + '7. Clean the dog areas\n',
+    'Tuesday': BASE_TASKS + '7. Clean the cat areas\n',
+    'Wednesday': BASE_TASKS + '7. Feed the aligator\n',
+    'Thursday': BASE_TASKS+ '7. Clean the dog areas\n',
+    'Friday': BASE_TASKS + '7. Clean the cat areas\n',
+    'Saturday': BASE_TASKS,
+    'Sunday': BASE_TASKS
+}
+
+MESSAGES = {
+    'come_to_work': ('Remember that you\'re scheduled for'
+                  + ' work today at 9am! If you\'re sick or need a '
+                  + 'replacement please reply to this email now!'),
+    'work_tasks': 'Remember that today is {}. Please be sure to: \n{}'.format(TODAY, DAILY_TASKS[TODAY]),
+    'pickup_your_pet': 'Remember to pickup your pet today by 8pm!'
+}
+# In the AWS SES sandbox you can only send to/from verified emails
+# Remember to do this before adding your emails here
+
+EMPLOYEES = recipients.recipients['employees']
+# Content stored in this order: [email, first_name, last_name]
+CLIENTS = recipients.recipients['clients']
+# Content stored in this order: [email, first_name, last_name, pet_name]
+
+def get_template(template_file='./template.html'):
+    # Load and return html template
+    try:
+        with open(template_file) as t_file:
+            raw_template = t_file.read()
+        template = Template(raw_template)
+    except Exception as e:
+        logger.info('failed to load template')
+        raise e
+    return template
+
+def render_messages(title, message):
+    datetime.date
+    html_email = get_template().render(
+                title = title, 
+                salutation = salutation, 
+                message = message
+                )
+    plain_text_email = 'Hello' + message
+
+    return html_email, plain_text_email
+    
+
+def send_email_reminder(title, salutation, message, recipient):
+    # Sends an email to
+    try:
+        ses = boto3.client('ses')
+        response = ses.send_email(
+            Source=FROM_ADDRESS,
+            Destination={
+                'ToAddresses': recipient,
+                'CcAddresses': [],
+                'BccAddresses': []
+            },
+            Message={
+                'Subject': {
+                    'Data': title,
+                },
+                'Body': {
+                    'Text': {
+                        'Data': 'This is a non-html version of the email'
+                    },
+                    'Html': {
+                        'Data': html_email
+                    }
+                }
+            },
+            ReplyToAddresses=[
+                REPLY_TO_ADDRESS,
+            ]
+        )
+    except Exception as e:
+        logger.info("Failed to send message via SES")
+        logger.info(e.message)
+        raise e
+
+
+
+
+def send_come_to_work_reminder
+
+
+def send_pickup_your_pet_reminder
+
+
+def send_feed_pets_reminder
+
+
+for client in clients:
+
+
+
+
+###### FIGURE THIS SHIT OUT
+
+
+
+
+def handler(event,context):
+    # If x
+
+class Context:
+    def __init__(self, **entries):
+        self.__dict__.update(entries)
+
+if __name__ == "__main__":
+    event = {}
+    context = Context(dict())
+    send_finance_kpis(event, **context)
---
+++ recipients.py
@@ -0,0 +1,18 @@
+# List of email addresses and basic info to merge into templates
+# In a more complete application we might loaded this in from a 
+# database service such as DynamoDB
+
+recipients = {
+    'clients' :
+        [
+            ['pluralsight.fernando@gmail.com', 'Fernando', 'Medina Corey', 'Riley'],
+            ['zoe.on.the.firefly@outlook.com', 'Zoë', 'Washburne', 'Firefly II']
+            # Add contact info here. Don't forget to add a comma! ^
+        ],
+    'employees':
+        [
+            ['springfield.homer@yahoo.com', 'Homer', 'Simpson']
+            # Add contact info arrays here. Don't forget to add a comma!
+        ]
+}
+
